678f4927b10151cbe0b3995adc6c66cb4f7f18f4
Akash kumbhakar <samcodeaccess@gmail.com>
2022-04-28 00:39:08 +0530

    initial commit

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,11 @@
+// in reducer state cannot be null it has to be an initial value
+export default (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            return action.payload;
+        case 'CREATE':
+            return [...posts, action.payload];
+        default:
+            return posts;
+    }
+}
