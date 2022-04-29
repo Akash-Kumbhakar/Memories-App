398b0727c399e046d95e1ff53371bf0f6bedbdf6
Akash kumbhakar <samcodeaccess@gmail.com>
2022-04-29 21:30:30 +0530

    added like delete update feature

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,36 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import FileBase from 'react-file-base64';
-import { useDispatch } from 'react-redux';
-import { createPost } from "../../actions/posts";
+import { useDispatch, useSelector } from 'react-redux';
+import { createPost, updatePost } from "../../actions/posts";
 
-import useStyles from './styles'
+import useStyles from './styles';
 
 
-
-const Form = () => {
+const Form = ({ currentId, setCurrentId }) => {
     const [postData, setPostData] = useState({
         creator: '', title: '', message: '', tags:
             '', selectedFile: ''
     });
+
+    const post = useSelector((state) => (currentId ? state.posts.find((p) => p._id === currentId) : null));
     const classes = useStyles();
 
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        if (post) setPostData(post);
+    }, [post]);
+
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        dispatch(createPost(postData));
+        if (currentId === 0) {
+            dispatch(createPost(postData));
+            clear();
+        } else {
+            dispatch(updatePost(currentId, postData));
+            clear();
+        }
     };
 
     const clear = () => {
-
+        setCurrentId(0);
+        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' })
     };
 
     return (
         <Paper className={classes.paper}>
             <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography varian="h6">Creating a Memory</Typography>
+                <Typography varian="h6">{currentId ? 'Editing' : 'Creating'}  a Memory</Typography>
                 <TextField
                     name="creator"
                     variant="outlined"
@@ -61,7 +73,7 @@ const Form = () => {
                     label="Tags"
                     fullWidth
                     value={postData.tags}
-                    onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+                    onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}
                 />
                 <div className={classes.fileInput}>
                     <FileBase
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,16 @@
 // in reducer state cannot be null it has to be an initial value
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'FETCH_ALL':
+        case DELETE:
+            return posts.filter((post) => post._id != action.payload);
+        case UPDATE:
+        case LIKE:
+            return posts.map((post) => (post._id === action.payload._id ? action.payload : post));
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         default:
             return posts;
